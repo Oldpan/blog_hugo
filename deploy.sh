513dff80d23f1da26e2eff5cac82a91216d68e98
oldpan <295484914@qq.com>
2025-09-14 19:16:27 +0800

    update host url

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ export https_proxy=http://127.0.0.1:7890
 touch public/.nojekyll
 
 hugo --minify
-echo blog.oldpan.me > public/CNAME
+echo oldpan.me > public/CNAME
 git subtree split --prefix public -b gh-pages-tmp
 git push -f origin gh-pages-tmp:gh-pages         
 git branch -D gh-pages-tmp         
