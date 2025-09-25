0cf38332075231b43c943f46659081c5a372bc02
oldpan <295484914@qq.com>
2025-09-25 23:16:13 +0800

    update

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-export https_proxy=http://127.0.0.1:7890
-touch public/.nojekyll
-
-hugo --minify
-echo blog.oldpan.me > public/CNAME
-git subtree split --prefix public -b gh-pages-tmp
-git push -f origin gh-pages-tmp:gh-pages         
-git branch -D gh-pages-tmp         
